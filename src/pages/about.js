a1d7245d45ffa4fe3e37fdc3034767a76ff8052e
Raj Rajhans <raj.rajhans18@vit.edu>
2020-05-22 21:58:08 +0530

    completed about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,8 @@ import React, { Component } from "react"
 import Layout from "../components/layout"
 import HerosectionPage from "../components/herosectionPage"
 import "../styles/about.scss"
-import {AboutSVG} from "../static/svgs"
+import {AboutSVG, AboutBlogSVG} from "../static/svgs"
+import Pic from "../static/raj-profile-pic.png"
 
 class About extends Component {
   render() {
@@ -17,30 +18,60 @@ class About extends Component {
               <div className="aboutRajText">
                 <div className="aboutRajTextHeading">
                   About Raj
+                  <div className={"aboutBorderBottom"}/>
                 </div>
 
                 <div className="aboutRajTextContent">
-                  About Raj Text Content
+                  <p>
+                    Raj is a Second Year Computer Engineering student at VIT, Pune. A tech-aficionado, as he likes to put it, Raj is also a freelance web designer and developer.
+                  </p>
+
+                  <p>
+                    As an aspiring software developer, you will find him speaking computerese, barely comprehensible to anyone else.
+                    But don’t let his overtly composed and calm visible delude you. He can easily grab your attention with his eccentric and uncanny theories about ‘anything’ in general.
+                    Raj is an active tech blogger as he feels explaining helps in understanding.
+                  </p>
+
+                  <p>
+                    When not raising pull requests on GitHub, you will find him lost in a world of thriller books and soothing music.
+                  </p>
                 </div>
               </div>
 
               <div className="aboutRajPhoto">
-                About Raj Photo Here
+                <img src={Pic} alt={"raj-rajhans"}/>
               </div>
             </div>
 
             <div className="aboutBlog">
               <div className="aboutBlogPhoto">
-                About blog photo here
+                <AboutBlogSVG/>
               </div>
 
               <div className="aboutBlogText">
                 <div className="aboutBlogTextHeading">
                   About the Blog
+                  <div className={"aboutBorderBottom"}/>
                 </div>
 
                 <div className="aboutBlogTextContent">
-                  About Blog Text Content
+
+                  <p>
+                    Oppenheimer once said, “The best way to learn is to teach”.
+                  </p>
+                  <p>
+                    Blogging is a way to document one’s learnings. If you haven’t, go watch <a href={"https://www.youtube.com/watch?v=RVKofRN1dyI"} target={"_blank"}> Gary Vaynerchuk’s Document, Don’t Create video.</a>
+                  </p>
+
+                  <p>
+                    Developers are creators, and documenting the creating process only improves the developer’s skills. Writing about something solidifies the learning process. It’s easy to say you’ve done some reading on X, but if you have a blog post and a GitHub project to back up, it really makes it more believable. Not just that, it has the potential to help someone. A small contribution is always better than no contribution.
+                  </p>
+
+                  <p>
+                    The aim of this blog is to document the projects, ideas, and the learnings from the projects that I would be working on.
+                  </p>
+
+
                 </div>
               </div>
             </div>

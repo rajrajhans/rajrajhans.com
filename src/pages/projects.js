412ee3fa04be83eb33bc4a3620313ae530dd43b1
Raj Rajhans <raj.rajhans18@vit.edu>
2020-06-03 23:40:29 +0530

    project box design done

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import "../styles/projects.scss"
 import HerosectionPage from "../components/herosectionPage"
 import {ProjectsSVG} from "../static/svgs"
+import projectimg from "../static/ss.png"
 
 class Projects extends Component {
   render() {
@@ -13,21 +14,31 @@ class Projects extends Component {
         <div className="content-container">
 
           <div className="projectBox">
-
+            <a href={"/"}>
             <div className="projectImage">
-              Image
+              <img src={projectimg} alt={"projectimg"}/>
+              <div className="projectImageOverlay">
+                <div className="projectImageIcon">
+                  <i className={"fa fa-link"}/>
+                </div>
+              </div>
             </div>
+            </a>
 
             <div className="projectTitle">
               title
             </div>
 
             <div className="projectDescription">
-              description
+             desc Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
             </div>
 
             <div className="projectLinks">
-              links
+              <a href={"google.com"}>
+                <div className="projectLink">
+                  <i className={"fa fa-github fa-lg"}/>
+                </div>
+              </a>
             </div>
 
             <div className="projectTags">

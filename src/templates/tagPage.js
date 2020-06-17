346a0286ca3ad2b5696b65864b767c08d7488a99
Raj Rajhans <raj.rajhans18@vit.edu>
2020-06-18 01:51:14 +0530

    Tag Page Blog Grid Done

--- src/templates/tagPage.js
+++ src/templates/tagPage.js
@@ -1,6 +1,9 @@
 import React from "react"
-
-import { Link, graphql } from "gatsby"
+import TagIcon from "../static/tagIcon"
+import CalendarIcon from "../static/calendarIcon"
+import Img from "gatsby-image"
+import "../styles/tagPage.scss"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import InternalLinkRight from "../components/utilComponents/internalLinkRight"
@@ -20,24 +23,45 @@ const Tags = ({ pageContext, data, location }) => {
       <SEO title={tagHeader} />
       <div className={"heroSection"}>
         <div className="titleSection">
-          <div className="title"><span className={"brand-color"}>{totalCount}</span> post{totalCount === 1 ? "" : "s"} tagged with {tag}</div>
+          <div className="title"><span className={"brand-color"}>{totalCount}</span> post{totalCount === 1 ? "" : "s"} tagged with "{tag}"</div>
         </div>
 
       </div>
-      <ul>
-        {edges.map(({ node }) => {
-          const { slug } = node.fields
-          const { title } = node.frontmatter
-          return (
-            <li key={slug}>
-              <InternalLinkDown link={slug}>{title}</InternalLinkDown>
-            </li>
-          )
-        })}
-      </ul>
+      <div className="homeBlogBoxesCntnr">
+        {edges.map((node, id) => {
+          const blog = node.node
+          return(<div className="homeBlogBox" key={id}>
+            <InternalLinkDown link={blog.fields.slug}>
+              <div className="homeBlogImage">
+                <Img fixed={blog.frontmatter.featuredImage.childImageSharp.fixed}/>
+              </div>
 
-      <InternalLink link={"/tags"}>
-        <div className={"blogPrevCntnr"}>
+              <div className="homeBlogTitle">
+                {blog.frontmatter.title}
+              </div>
+
+              <div className="homeBlogDateTag">
+              <span className={"homeBlogDate"}>
+                <CalendarIcon/>
+                <span> {blog.frontmatter.date}</span>
+              </span>
+
+                <span className={"homeBlogTag"}>
+                <TagIcon/>
+                <span> {blog.frontmatter.mainTag}</span>
+              </span>
+              </div>
+
+              <div className="homeBlogExcerpt">
+                {blog.frontmatter.description}
+              </div>
+            </InternalLinkDown>
+          </div>)
+        })}
+      </div>
+
+      <InternalLink link={"/archives"}>
+        <div className={"blogPrevCntnr allTagsLinkCntnr"}>
           <svg className={"blogPrevSVG"} stroke="#3355ff" fill="#3355ff" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em"><path d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"></path></svg>
           All Tags
         </div>
@@ -68,6 +92,16 @@ export const pageQuery = graphql`
           }
           frontmatter {
             title
+            description
+            date (formatString: "MMMM Do, YYYY")
+            mainTag
+            featuredImage {
+              childImageSharp {
+                fixed(width: 300) {
+                  ...GatsbyImageSharpFixed_withWebp
+                }
+              }
+            }
           }
         }
       }

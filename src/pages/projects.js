2be1ab552afd595a30a35009c73c4522726e1eec
Raj Rajhans <raj.rajhans18@vit.edu>
2020-06-28 13:59:35 +0530

    Projects page content added

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -11,6 +11,7 @@ class Projects extends Component {
   render() {
 
     const data = this.props.data.allProjectsJson.nodes;
+    const webdevdata = this.props.data.allWebdevJson.nodes;
 
     return (
       <Layout location={this.props.location} customclass={"projectsWrapper"}>
@@ -23,10 +24,9 @@ class Projects extends Component {
         <HerosectionPage title={"Projects"} svg={<ProjectsSVG/>}/>
 
         <div className="content-container projectBox_container">
-
           {data.map((node, index) => (
             <div className="projectBox" key={node.id}>
-              <a href={"/"}>
+              <a className={"nostylelink"} rel={"noopener noreferrer"} target={"_blank"}  href={node.website ? node.website : (node.casestudy ? node.casestudy : node.github)}>
                 <div className="projectImage">
                   {/*<img src={projectimg} alt={"projectimg"}/>*/}
                   <Img fluid={node.image.childImageSharp.fluid}/>
@@ -40,7 +40,9 @@ class Projects extends Component {
               </a>
 
               <div className="projectTitle">
-                <h2>{node.title}</h2>
+                <a className={"nostylelink"} rel={"noopener noreferrer"} target={"_blank"}  href={node.casestudy ? node.casestudy : (node.website ? node.website : node.github)}>
+                  <h2>{node.title}</h2>
+                </a>
               </div>
 
               <div className="projectSubtitle">
@@ -51,6 +53,70 @@ class Projects extends Component {
 
               <div className="projectDescription">
                 {node.description}
+              </div>
+
+              <div className="projectLinks">
+                {node.github ? (
+                  <a href={node.github} title={"View Code on GitHub"}  target={"_blank"} rel={"noopener noreferrer"}>
+                    <div className="projectLink">
+                      <i className={"fa fa-github fa-lg"}/>
+                    </div>
+                  </a>
+                ): null}
+
+                {node.casestudy ? (
+                  <a href={node.casestudy} title={"View Case Study"} target={"_blank"} rel={"noopener noreferrer"}>
+                    <div className="projectLink">
+                      <i className={"fa fa-file-text fa-lg"} style={{fontSize:"27px"}}/>
+                    </div>
+                  </a>
+                ): null}
+
+                {node.website ? (
+                  <a href={node.website} title={"View Live Version"} target={"_blank"} rel={"noopener noreferrer"}>
+                    <div className="projectLink">
+                      <i className={"fa fa-globe fa-lg"} style={{fontSize:"30px"}}/>
+                    </div>
+                  </a>
+                ): null}
+              </div>
+
+              <div className="projectTags">
+                {node.tags.map((tag)=>(
+                  "#"+tag+"  "
+                ))}
+              </div>
+
+            </div>
+          ))}
+        </div>
+
+        <div className="webdevtext">Freelance / Website Design & Development Projects<div className={"aboutBorderBottom"}/></div>
+        <div className="content-container projectBox_container">
+          {webdevdata.map((node, index) => (
+            <div className="projectBox" key={node.id}>
+              <a className={"nostylelink"} rel={"noopener noreferrer"} target={"_blank"}  href={node.website ? node.website : (node.casestudy ? node.casestudy : node.github)}>
+                <div className="projectImage">
+                  {/*<img src={projectimg} alt={"projectimg"}/>*/}
+                  <Img fluid={node.image.childImageSharp.fluid}/>
+
+                  <div className="projectImageOverlay">
+                    <div className="projectImageIcon">
+                      <i className={"fa fa-link"}/>
+                    </div>
+                  </div>
+                </div>
+              </a>
+
+              <div className="projectTitle">
+                <a className={"nostylelink"} rel={"noopener noreferrer"} target={"_blank"}  href={node.casestudy ? node.casestudy : (node.website ? node.website : node.github)}>
+                  <h2>{node.title}</h2>
+
+                </a>
+              </div>
+
+              <div className="projectDescription webdevDescription">
+                <span dangerouslySetInnerHTML={{__html: node.description}}/>
               </div>
 
               <div className="projectLinks">
@@ -114,6 +180,22 @@ export const query = graphql`
         website
         github
         casestudy
+        tags
+      }
+    }
+    allWebdevJson {
+      nodes {
+        id
+        title
+        description
+        image{
+              childImageSharp {
+                fluid(maxWidth: 400) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+              }
+        website
         tags
       }
     }

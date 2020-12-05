f31e662b9b0518d32bf5eeccb7543433fb06cb7b
Raj Rajhans <raj.rajhans18@vit.edu>
2020-12-05 22:25:10 +0530

    Fix TooMuchRecursionError - Override react-helmet's shouldComponentUpdate function using lodash

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,12 @@
 import "prismjs/themes/prism.css"
 import "./src/styles/global.scss"
 import  "./node_modules/font-awesome/css/font-awesome.css"
+
+import {isEqual} from 'lodash'
+import { Helmet } from "react-helmet"
+
+// This is a fix for "InternalError: Too much recursion". React Helmet uses deep-equal and sometimes crashes on circular objects. So, I'm replacing it with lodash's isEqual, which provides protection again circular references.
+
+Helmet.prototype.shouldComponentUpdate = function(nextProps) {
+  return !isEqual(this.props, nextProps)
+}

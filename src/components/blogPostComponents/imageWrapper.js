c27f668720a720d1f32f6a9d357d010b4592450c
RajRajhans <me@rajrajhans.com>
2021-04-20 15:16:29 +0530

    (dev): update styles for ImageWrapper and transparent images

--- src/components/blogPostComponents/imageWrapper.js
+++ src/components/blogPostComponents/imageWrapper.js
@@ -2,12 +2,17 @@ import React, { Component } from "react";
 
 class ImageWrapper extends Component {
   width = this.props.width ? this.props.width : "60%";
+  boxShadow = this.props.isTransparent ? "none" : null;
 
   render() {
     return (
       <div
         className={"bpstImgWrapper"}
-        style={{ width: this.width, margin: "auto" }}
+        style={{
+          width: this.width,
+          margin: "auto",
+          boxShadow: this.boxShadow,
+        }}
       >
         {this.props.children}
       </div>

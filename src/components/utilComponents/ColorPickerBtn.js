a0cdc61f9e9d87f1bc97bda699534a5cc8f52fcf
RajRajhans <me@rajrajhans.com>
2021-06-22 15:05:44 +0530

    Add Sketch color picker to navbar

--- src/components/utilComponents/ColorPickerBtn.js
+++ src/components/utilComponents/ColorPickerBtn.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { SketchPicker } from "react-color";
 
 const ColorPickerBtn = () => {
+  const [isColorPickerOpen, setIsColorPickerOpen] = useState(false);
+  const [selectedThemeColor, setSelectedThemeColor] = useState("#3355ff");
+
+  const handleColorPickerVisibilityChange = () => {
+    setIsColorPickerOpen((prevState) => !prevState);
+  };
+
+  const handleThemeColorChange = (color) => {
+    setSelectedThemeColor(color.hex);
+  };
+
   return (
-    <div className={"nav-color-picker"}>
-      <i className={"fa fa-eyedropper fa-2x"} />
+    <div className={"nav-color-picker-container"}>
+      <i
+        className={"fa fa-eyedropper fa-2x nav-color-picker"}
+        onClick={handleColorPickerVisibilityChange}
+      />
+      <div className="nav-color-picker-picker">
+        <SketchPicker
+          color={selectedThemeColor}
+          onChangeComplete={handleThemeColorChange}
+          onChange={handleThemeColorChange}
+        />
+      </div>
     </div>
   );
 };

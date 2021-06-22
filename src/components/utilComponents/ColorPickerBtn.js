6e8580a203d170249986f71fce973f47bf7de3be
RajRajhans <me@rajrajhans.com>
2021-06-22 14:47:40 +0530

    add eyedropper icon on the navbar

---
+++ src/components/utilComponents/ColorPickerBtn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ColorPickerBtn = () => {
+  return (
+    <div className={"nav-color-picker"}>
+      <i className={"fa fa-eyedropper fa-2x"} />
+    </div>
+  );
+};
+
+export default ColorPickerBtn;
